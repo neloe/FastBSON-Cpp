11f0e19aa392a40bcad3c3bc73d62b912dd5b223
Nathan Eloe <powerofazure@gmail.com>
2014-07-24 15:27:37 -0500

    convert_utils.h documentation

--- template_spec/convert_utils.h
+++ template_spec/convert_utils.h
@@ -13,6 +13,11 @@
 
 namespace bson
 {
+  /*!
+   * \brief converts a value directly to its byte representation
+   * \pre the bytes array has at least sizeof(T) bytes allocated
+   * \post copies the bytes contained in data to the bytes array
+   */
   template <class T>
   void _to_bytes(char* bytes, const T& data)
   {
@@ -20,12 +25,24 @@ namespace bson
     return;
   }
   
+  /*!
+   * \brief creates a void shared pointer from the provided data
+   * \pre None
+   * \post None
+   * \return a shared void pointer pointing at the data
+   */
   template <class T>
   std::shared_ptr<void> make_void_shared(const T& data)
   {
     return std::static_pointer_cast<void>(std::make_shared<T>(data));
   }
   
+  /*!
+   * \brief converts a byte array to type T (direct copying)
+   * \pre bytes has at least sizeof(T) spaces allocated)
+   * \post None
+   * \return the data that is the bytes in the array, but as type T
+   */
   template <class T>
   T _from_bytes(const char* bytes)
   {
@@ -36,6 +53,11 @@ namespace bson
     return data;
   }
   
+  /*!
+   * \brief inserts the bytes of object v in into the stream
+   * \pre None
+   * \post the byte representation of object v is inserted into the stream
+   */
   template <class T>
   void _to_stream(std::ostringstream& ss, const T& v)
   {
@@ -44,6 +66,11 @@ namespace bson
       ss << *it;
   }
   
+  /*!
+   * \brief outputs the bson contained in the stream to the screen (for debug)
+   * \pre None
+   * \post the byte representation of stream is output to the screen
+   */
   void printbson(std::ostringstream& ss)
   {
     for (int i=0; i<ss.tellp(); i++)
